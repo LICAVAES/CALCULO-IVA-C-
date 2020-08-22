80a7aeaf483fb084c38c0f3a629dabdd67d5dabd
CATALINA VANEGAS <licavaes@gmail.com>
2020-08-22 17:15:49 -0500

    se realiza ejercicio para el calculo del iva taller 1

---
+++ Form1.Designer.cs
@@ -0,0 +1,192 @@
+﻿namespace CALCULO_IVA
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.BtnSalir = new System.Windows.Forms.Button();
+            this.BtnLimpiar = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.TxtPrecio = new System.Windows.Forms.TextBox();
+            this.Lblva = new System.Windows.Forms.Label();
+            this.LblTotalPagar = new System.Windows.Forms.Label();
+            this.BtnCalbular = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(210, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(233, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "CALCULO DEL IVA";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 94);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "PRECIO";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 134);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(24, 13);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "IVA";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(11, 175);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(92, 13);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "TOTAL A PAGAR";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // BtnSalir
+            // 
+            this.BtnSalir.Location = new System.Drawing.Point(165, 278);
+            this.BtnSalir.Name = "BtnSalir";
+            this.BtnSalir.Size = new System.Drawing.Size(121, 34);
+            this.BtnSalir.TabIndex = 3;
+            this.BtnSalir.Text = "SALIR";
+            this.BtnSalir.UseVisualStyleBackColor = true;
+            this.BtnSalir.Click += new System.EventHandler(this.BtnSalir_Click);
+            // 
+            // BtnLimpiar
+            // 
+            this.BtnLimpiar.Location = new System.Drawing.Point(15, 278);
+            this.BtnLimpiar.Name = "BtnLimpiar";
+            this.BtnLimpiar.Size = new System.Drawing.Size(121, 34);
+            this.BtnLimpiar.TabIndex = 2;
+            this.BtnLimpiar.Text = "LIMPIAR";
+            this.BtnLimpiar.UseVisualStyleBackColor = true;
+            this.BtnLimpiar.Click += new System.EventHandler(this.BtnLimpiar_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::CALCULO_IVA.Properties.Resources.iva;
+            this.pictureBox1.Location = new System.Drawing.Point(345, 86);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(263, 166);
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // TxtPrecio
+            // 
+            this.TxtPrecio.Location = new System.Drawing.Point(109, 91);
+            this.TxtPrecio.Name = "TxtPrecio";
+            this.TxtPrecio.Size = new System.Drawing.Size(113, 20);
+            this.TxtPrecio.TabIndex = 0;
+            // 
+            // Lblva
+            // 
+            this.Lblva.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Lblva.Location = new System.Drawing.Point(144, 134);
+            this.Lblva.Name = "Lblva";
+            this.Lblva.Size = new System.Drawing.Size(78, 20);
+            this.Lblva.TabIndex = 1;
+            this.Lblva.Text = "0";
+            this.Lblva.Visible = false;
+            // 
+            // LblTotalPagar
+            // 
+            this.LblTotalPagar.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblTotalPagar.Location = new System.Drawing.Point(144, 175);
+            this.LblTotalPagar.Name = "LblTotalPagar";
+            this.LblTotalPagar.Size = new System.Drawing.Size(78, 20);
+            this.LblTotalPagar.TabIndex = 1;
+            this.LblTotalPagar.Text = "0";
+            this.LblTotalPagar.Visible = false;
+            this.LblTotalPagar.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // BtnCalbular
+            // 
+            this.BtnCalbular.Location = new System.Drawing.Point(73, 218);
+            this.BtnCalbular.Name = "BtnCalbular";
+            this.BtnCalbular.Size = new System.Drawing.Size(134, 34);
+            this.BtnCalbular.TabIndex = 1;
+            this.BtnCalbular.Text = "CALCULAR";
+            this.BtnCalbular.UseVisualStyleBackColor = true;
+            this.BtnCalbular.Click += new System.EventHandler(this.BtnCalbular_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(643, 341);
+            this.ControlBox = false;
+            this.Controls.Add(this.BtnCalbular);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.BtnLimpiar);
+            this.Controls.Add(this.BtnSalir);
+            this.Controls.Add(this.TxtPrecio);
+            this.Controls.Add(this.LblTotalPagar);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.Lblva);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button BtnCalcular;
+        private System.Windows.Forms.Button BtnSalir;
+        private System.Windows.Forms.Button BtnLimpiar;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox TxtPrecio;
+        private System.Windows.Forms.Label Lblva;
+        private System.Windows.Forms.Label LblTotalPagar;
+        private System.Windows.Forms.Button BtnCalbular;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CALCULO_IVA
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+               
+        private void BtnCalbular_Click(object sender, EventArgs e)
+        {
+            double precio = double.Parse(TxtPrecio.Text);
+            double totalPagar, iva;
+            iva = precio * 0.19;
+            totalPagar= precio + iva;
+            Lblva.Visible = true;
+            Lblva.Text = Convert.ToString(iva);
+            LblTotalPagar.Visible = true;
+            LblTotalPagar.Text = Convert.ToString(totalPagar);
+
+         
+        }
+
+        private void BtnLimpiar_Click(object sender, EventArgs e)
+        {
+            TxtPrecio.Text = String.Empty;
+            TxtPrecio.Focus();
+            Lblva.Visible = false;
+            LblTotalPagar.Visible = false;
+
+
+           }
+
+        private void BtnSalir_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
